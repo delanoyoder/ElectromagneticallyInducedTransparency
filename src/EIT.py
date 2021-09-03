d11d9c1f2bcf2b4e646eb0a9cb4e326e5b3be4c2
Del <dayoder4@gmail.com>
2021-09-03 10:44:21 -0500

    Restructured EIT class and added TLS class

--- src/EIT.py
+++ src/EIT.py
@@ -6,58 +6,101 @@ import matplotlib.pyplot as plt
 
 class TLS:
 
-    def __init__(self, t, c_1, c_2, c_3):
-        self.t = t
-        self.c_1 = c_1
-        self.c_2 = c_2
-        self.c_3 = c_3
-
-class EIT:
-
     def __init__(self):
-        self.DE = complex_ode(self.three_level_system)
-
-    def three_level_system(self, t, c, A=[50,50], probe_offset=5):
-        
-        c_1, c_2, c_3 = c
-        A_1, A_2 = A
-        
-        O_c = A_1 * (np.tanh(t-1) + 1)
-        O_p = A_2 * (np.tanh(t-1 - probe_offset) + 1)
-        
-        dcdt = [(i / 2 * (O_p * c_3))]
-        dcdt.append((i / 2 * (O_c * c_3)))
-        dcdt.append((i / 2 * (O_p * c_1 + O_c * c_2)))
-    
-        return dcdt
-    
-    def simulate(self, populations_0=[1,0,0], t_0=0, t_f=10, dt=0.25):
-
-        self.DE.set_initial_value(populations_0, t_0)
-
+        self.t = []
         self.c_1 = []
         self.c_2 = []
         self.c_3 = []
-        self.t = []
-        while self.DE.successful() and self.DE.t < t_f:
-            self.DE.integrate(self.DE.t+dt)
-            self.c_1.append(abs(self.DE.y[0]))
-            self.c_2.append(abs(self.DE.y[1]))
-            self.c_3.append(abs(self.DE.y[2]))
-            self.t.append(self.DE.t)
+        self.O_c = []
+        self.O_p = []
 
-    def simulate_phase_transfer(self):
-        pass
-
-    def plot_dark_state(self):
+    def plot_DSS(self):
+        """
         plt.figure(figsize=(7,5))    
-        plt.plot(self.t,self.c_1)
-        plt.plot(self.t,self.c_2,'--')
-        plt.plot(self.t,self.c_3,'.')
+        plt.plot(self.t, self.c_1)
+        plt.plot(self.t, self.c_2,'--')
+        plt.plot(self.t, self.c_3,'.')
+        plt.plot(self.t, self.O_c)
+        plt.plot(self.t, self.O_p)
         plt.xlabel('Time')
         plt.ylabel('Population')
-        plt.legend([r'$c_{1}$',r'$c_{2}$',r'$c_{3}$'])
+        plt.legend([r'$c_{1}$', r'$c_{2}$', r'$c_{3}$', r'$Ω_{C}$', r'$Ω_{P}$'])
+        plt.savefig('DarkStateSimultation.pdf')
+        plt.show()
+        """
+
+        fig, ax1 = plt.subplots()
+        fig.set_figheight(5)
+        fig.set_figwidth(7)
+
+        ax1.set_xlabel('Time')
+        ax1.set_ylabel('Population')
+        ax1.plot(self.t, self.c_1, 'k')
+        ax1.plot(self.t, self.c_2,'b--')
+        ax1.plot(self.t, self.c_3,'g.')
+        ax1.tick_params(axis='y')
+        ax1.legend([r'$c_{1}$', r'$c_{2}$', r'$c_{3}$'], loc=2)
+
+        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+        ax2.set_ylabel(r'$Ω$ (arb. units)')  # we already handled the x-label with ax1
+        ax2.plot(self.t, self.O_c, 'r:', alpha=0.5)
+        ax2.plot(self.t, self.O_p, 'y-.', alpha=0.5)
+        ax2.tick_params(axis='y')
+        ax2.legend([r'$Ω_{C}$', r'$Ω_{P}$'], loc=1)
+
+        fig.tight_layout()  # otherwise the right y-label is slightly clipped
         plt.show()
 
-    def plot_phase_transfer(self):
-        pass
+class EIT:
+
+    def __init__(self, t_0, t_f, dt, c_0, t_c, A_c, t_p, A_p, TLS):
+        self.t_0 = t_0
+        self.t_f = t_f
+        self.dt = dt
+        self.c_0 = c_0
+        self.t_c = t_c
+        self.t_p = t_p
+        self.A_c = A_c
+        self.A_p = A_p
+        self.TLS = TLS
+
+        self.ode = complex_ode(self.diff_eq)
+
+    def diff_eq(self, t, c):
+        
+        c_1 = (i/2) * (self.probe_laser(t) * c[2])
+        c_2 = (i/2) * (self.coupling_laser(t) * c[2])
+        c_3 = (i/2) * (self.probe_laser(t) * c[0] + self.coupling_laser(t) * c[1])
+        
+        return [c_1, c_2, c_3]
+
+    def coupling_laser(self, t):
+        return self.A_c * (np.tanh(t - self.t_c - np.pi) + 1)
+
+    def probe_laser(self, t):
+        return self.A_p * (np.tanh(t - self.t_p - np.pi) + 1)
+
+    
+    def DSS(self):
+        self.ode.set_initial_value(self.c_0, self.t_0)
+
+        while self.ode.successful() and self.ode.t < self.t_f:
+
+            self.ode.integrate(self.ode.t + self.dt)
+
+            self.TLS.t.append(self.ode.t)
+            self.TLS.c_1.append(abs(self.ode.y[0]))
+            self.TLS.c_2.append(abs(self.ode.y[1]))
+            self.TLS.c_3.append(abs(self.ode.y[2]))
+
+        self.TLS.O_c = self.coupling_laser(np.array(self.TLS.t))
+        self.TLS.O_p = self.probe_laser(np.array(self.TLS.t))
+
+"""
+from EIT import *
+b = TLS()
+a = EIT(0, 10, 0.25, [1,0,0], 0, 50, np.pi, 50, b)
+a.DSS()
+b.plot_DSS()
+"""
